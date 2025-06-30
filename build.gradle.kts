35dd1969f280b0a9f80626b5d8978a6af7dc589f
Duncan Calvert <dcalvert@ihmc.org>
2025-06-30 14:18:38 -0500

    Address PR comments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.0"
+   version = "0.1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-alexander-sdk"
    openSource = true
 
-   resourceDirectory("main", "../..")
+   resourceDirectory("main", "../..") // Include all the SDK content as classpath resources
    // resourceDirectory("main", "../../alexander-models") TODO: Probably better to switch to these
    // resourceDirectory("main", "../../alexander-ros2-api") TODO: Probably better to switch to these
    configurePublications()
